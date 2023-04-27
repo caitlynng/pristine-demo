d9d257d32ff5d8d8142d5a2357a0e40186031fae
catbouce <75095386+catbouce@users.noreply.github.com>
2023-04-26 21:46:41 -0700

    update landing page

--- client/src/assets/wrappers/LandingPage.js
+++ client/src/assets/wrappers/LandingPage.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div`
+  min-height: 100vh;
   header {
     z-index: 1000;
     position: sticky;
@@ -39,6 +40,7 @@ const Wrapper = styled.div`
   }
   .wrapper {
     max-width: 100vw;
+    margin-bottom: 3em;
   }
   .section-container {
     margin: 2em 0;
@@ -70,68 +72,34 @@ const Wrapper = styled.div`
   .banner-text {
     margin: 25px auto;
     width: 75%;
+    &:before {
+      content: "";
+      display: block;
+      position: relative;
+      margin: 0 auto;
+      margin-bottom: 25px;
+      width: 92px;
+      height: 1.4px;
+      background: #000;
+    }
   }
   .banner-img {
-    display: none;
-  }
-  .functions-container {
-    box-shadow: var(--shadow-dark);
-    padding: 2em;
-    border-radius: 1em;
-    line-height: 2em;
-    z-index: 1;
-    background-color: white;
-    position: relative;
-    text-align: center;
-  }
-  .cubicle {
-    flex: 1;
-    text-align: center;
-    & p {
-      margin-top: 1em;
+    display: block;
+    width: 90%;
+    padding-bottom: 3em;
+    padding-top: 3em;
+    & img {
+      width: 100%;
+      z-index: 100;
+      position: relative;
     }
   }
-  .functions-content {
-    margin: 4em 2em;
-    gap: 6em;
-  }
-  .section-intro {
-    margin: 2em auto;
-    text-align: center;
-    & p {
-      font-size: 18px;
-    }
-  }
-  .section-title {
-    color: black;
-    font-weight: 600;
-    font-family: "Inter", sans-serif;
-    & b {
-      font-weight: 900;
-    }
-  }
-  .feature-content {
-    padding: 1em;
-    & h3 {
-      font-size: 2em;
-      margin-left: 0.5em;
-    }
-    & svg {
-      font-size: 2em;
-      fill: #0c8b8d;
-    }
-    & .fill-none {
-      fill: none;
-    }
-    & .dark {
-      color: #773509;
-    }
-    & .color-2 {
-      color: #0c8b8d;
-    }
-    & .color-3 {
-      color: #410497;
-    }
+  footer {
+    background-color: var(--blue-700);
+    margin-top: 3em;
+    height: 3em;
+    color: var(--grey-400);
+    margin-top: auto;
   }
 
   @media (min-width: 1000px) {
@@ -146,18 +114,6 @@ const Wrapper = styled.div`
         background-color: #e5ffff;
         z-index: 0;
       }
-      /* &:before {
-        content: " ";
-        height: 50vh;
-        position: absolute;
-        top: 120vh;
-        left: 0;
-        width: 100vw;
-        background-color: #eef5ff;
-        z-index: 0;
-        border-bottom-left-radius: 5em;
-        border-bottom-right-radius: 5em;
-      } */
     }
     .section-container {
       max-width: 1616px;
@@ -169,101 +125,24 @@ const Wrapper = styled.div`
       display: flex;
     }
     .banner-heading {
-      text-align: left;
-      width: 40%;
+      text-align: center;
+      width: 57%;
+      z-index: 10;
       & h1 {
-        font-size: 3em;
-        line-height: 55px;
+        font-size: 5em;
+        line-height: 65px;
       }
     }
     .banner-img {
-      display: block;
-      width: 45%;
-      padding-bottom: 3em;
-      padding-top: 3em;
+      width: 60%;
       & img {
-        width: 100%;
-        z-index: 100;
-        position: relative;
-        border: 3px solid black;
-        border-top-width: 1.75em;
-        border-radius: 10px;
+        transform: translateX(0) translateY(0) rotate(0) skewX(0) skewY(0)
+          scaleX(1.1) scaleY(1.1);
       }
     }
     .banner-text {
       width: 100%;
       font-size: 20px;
-      &:before {
-        content: "";
-        display: block;
-        position: relative;
-        margin-bottom: 25px;
-        width: 92px;
-        height: 1.4px;
-        background: #000;
-      }
-    }
-    .functions-container,
-    .feature-content-wrapper {
-      width: 90%;
-    }
-    .bgc-bot {
-      background-color: #e5ffff;
-      padding-bottom: 7em;
-      border-bottom-right-radius: 5em;
-      position: relative;
-      &:before {
-        content: " ";
-        height: 15em;
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100vw;
-        background-color: white;
-        z-index: 0;
-      }
-    }
-    .functions-content {
-      flex-direction: row;
-    }
-    .feature-content-wrapper {
-      margin: 7em auto;
-    }
-    .feature-bg {
-      border-radius: 3.2em;
-      padding: 3em 1em;
-    }
-    .bgc-1 {
-      background-color: #e5ffff;
-    }
-    .bgc-2 {
-      background-color: #f9f5ff;   
-    }
-    .feature-img-container {
-      &.grid {
-        display: grid;
-      }
-    }
-    .feature-content {
-      & h3 {
-        font-size: 2.5em;
-      }
-      & svg {
-        font-size: 3em;
-      }
-    }
-    .features-item {
-      flex-direction: row;
-    }
-  }
-  @media (min-width: 1200px) {
-    .feature-content-wrapper {
-      flex-direction: row;
-    }
-    .feature-content {
-      &.padding-6 {
-        padding-left: 6em;
-      }
     }
   }
 `;
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,79 +1,29 @@
-import { useState, useEffect, useRef } from "react";
 import Wrapper from "../assets/wrappers/LandingPage";
 import { useNavigate } from "react-router-dom";
-import { Logo, Button } from "../components/index.js";
-import { IoIosAnalytics } from "react-icons/io";
-import {HiOutlineReceiptTax} from 'react-icons/hi'
-import {TbReportSearch} from 'react-icons/tb'
-import SVG from "react-inlinesvg";
-import homeBanner from "../assets/images/home_banner.png";
-import uploadIcon from "../assets/images/upload-icon.svg";
-import costIcon from "../assets/images/cost-icon.svg";
-import chartIcon from "../assets/images/chart-icon.svg";
-import analytics from "../assets/images/analytics.svg";
-import statements from "../assets/images/statements.svg";
-import report from "../assets/images/report.svg";
-
-//https://www.emburse.com/
-//https://quickbooks.intuit.com/
-//https://www.rydoo.com/expense/
-//https://www.ynab.com/
-//https://goodbudget.com/what-you-get/
+import { Logo, Button} from "../components/index.js";
+import homeBanner from "../assets/images/banner.png";
 
 const Landing = () => {
-  const navigate = useNavigate()
-  const [scroll, setScroll] = useState(false);
-  const scrollToRef = [];
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      setScroll(window.scrollY > 56);
-    });
-  }, []);
-
-  const executeScroll = (ind) => {
-    scrollToRef[ind].scrollIntoView({ behavior: "smooth" });
-    //to prevent window.scrollBy from cancelling on scrollIntoView if the latter hasn't finished scrolling before former runs
-    setTimeout(() => {
-      window.scrollBy(0, -80);
-    }, 500);
-  };
-  const navigateToDemo = () => navigate("/demo")
+  const navigate = useNavigate();
+  const navigateToDemo = () => navigate("/demo");
+  
   return (
-    <Wrapper>
-      <header className={scroll ? "header-fixed" : ""}>
+    <Wrapper className="flex flex-column">
+      <header>
         <nav>
           <div className="left-nav flex align-center">
             <Logo />
-            <Button
-              title="How it works"
-              classList="plain-btn hide-smscreen show-1000mw nav-btn"
-              onSetActive={() => executeScroll(0)}
-            />
-            <Button
-              title="What you get"
-              classList="plain-btn hide-smscreen show-1000mw nav-btn"
-              onSetActive={() => executeScroll(1)}
-            />
           </div>
-          <Button
-            title="View A Demo"
-            classList="contact-btn clear-btn hide-smscreen show-850mw"
-            onSetActive={() => navigateToDemo()}
-          />
-          <Button
-            title="Contact Us"
-            classList="contact-btn hide-smscreen show-850mw"
-          />
         </nav>
       </header>
       <div className="wrapper">
         <section className="section-container">
           <div
-            className={`w-full align-center justify-between banner-container`}
+            className={`w-full align-center justify-center banner-container`}
           >
             <div className="banner-heading">
               <h1>
-                Save Time, Track Money, and Get{" "}
+                Save Time, Track Money, and Get <br></br>
                 <span className="gradient-text">Important Insights</span>
               </h1>
               <p className="banner-text">
@@ -81,116 +31,21 @@ const Landing = () => {
                 and keeps all aspects of your accounting process running
                 smoothly.
               </p>
-              <Button title="View a Demo" classList="contact-btn" />
-            </div>
-            <div className="banner-img">
-              <img src={homeBanner}></img>
-            </div>
-          </div>
-        </section>
-        {/* how it works */}
-        <section
-          className="section-container"
-          ref={(el) => (scrollToRef[0] = el)}
-        >
-          <div className="functions-container flex flex-column align-center justify-between margin-auto">
-            <div className="section-intro">
-              <h2 className="section-title">
-                Experience <b>less</b> financial stress
-              </h2>
-              <p>
-                Pristine lets you keep track of your sales and expenses in 3
-                simple steps
-              </p>
-            </div>
-            <div className="w-full flex functions-content flex-column">
-              <div className="cubicle">
-                <SVG src={uploadIcon} alt="upload-icon" />
-                <p>Upload your reports from Paypal and third-party shippings</p>
-              </div>
-              <div className="cubicle">
-                <SVG src={costIcon} alt="total-cost-icon" />
-                <p>Input total cost of goods sold for the selected period</p>
-              </div>
-              <div className="cubicle">
-                <SVG src={chartIcon} alt="chart-icon" />
-                <p>Get insights of how your business is performing</p>
-              </div>
+              <Button
+                title="View a Demo"
+                classList="contact-btn"
+                onSetActive={navigateToDemo}
+              />
             </div>
           </div>
         </section>
-        {/* what you get */}
-        <section
-          className="section-container"
-          ref={(el) => (scrollToRef[1] = el)}
-        >
-          <div className="features-container w-full">
-            <div className="section-intro">
-              <h2 className="section-title">
-                Stay in complete <b>control</b>
-              </h2>
-              <p>
-                Pristine lets you keep track of your sales and expenses in 3
-                simple steps
-              </p>
-            </div>
-
-            <div className="feature-content-wrapper flex align-center flex-column-rev feature-bg bgc-1">
-              <div className="feature-img-container hide-smscreen show-850mw flex-2 grid grid-center">
-                <SVG src={analytics} alt="dashboard-img" />
-              </div>
-              <div className="feature-content flex-1">
-                <div className="flex align-center row-margin-2">
-                  <IoIosAnalytics className="color-2"/>
-                  <h3 className="color-2">Analytics</h3>
-                </div>
-                <p>
-                  Test your native hybrid of web mobile apps using LambdaTest’s
-                  online real device cloud and virtual mobile device platform of
-                  emulators and simulators. Eliminating your in-house device
-                  labs and bring high scalability to your mobile app testing.
-                </p>
-              </div>
-            </div>
-
-            <div className="feature-content-wrapper flex align-center flex-column">
-              <div className="feature-content flex-1 padding-6">
-                <div className="flex align-center row-margin-2">
-                  <HiOutlineReceiptTax className="fill-none dark"/>
-                  <h3 className="dark">Statements</h3>
-                </div>
-                <p>
-                  Test your native hybrid of web mobile apps using LambdaTest’s
-                  online real device cloud and virtual mobile device platform of
-                  emulators and simulators. Eliminating your in-house device
-                  labs and bring high scalability to your mobile app testing.
-                </p>
-              </div>
-              <div className="feature-img-container hide-smscreen show-850mw grid grid-center flex-1">
-                <img src={statements}></img>
-              </div>
-            </div>
-            
-            <div className="feature-content-wrapper flex align-center flex-column-rev feature-bg bgc-2">
-              <div className="feature-img-container hide-smscreen show-850mw flex-2 grid grid-center">
-                <img src={report} alt="dashboard-img" />
-              </div>
-              <div className="feature-content flex-1">
-                <div className="flex align-center row-margin-2">
-                  <TbReportSearch className="color-3 fill-none"/>
-                  <h3 className="color-3">Reports</h3>
-                </div>
-                <p>
-                  Test your native hybrid of web mobile apps using LambdaTest’s
-                  online real device cloud and virtual mobile device platform of
-                  emulators and simulators. Eliminating your in-house device
-                  labs and bring high scalability to your mobile app testing.
-                </p>
-              </div>
-            </div>
-          </div>
-        </section>
+        <div className="banner-img flex align-center w-full margin-auto">
+          <img src={homeBanner} alt="dashboard-screenshot"></img>
+        </div>
       </div>
+      <footer className="grid grid-center">
+        <p className="flex-1">All rights reserved</p>
+      </footer>
     </Wrapper>
   );
 };
